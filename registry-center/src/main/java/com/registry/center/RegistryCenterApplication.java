1df7418bd9a5d98763108b928c698cd8cf70f3e6
林泽锋 <282245889@qq.com>
2018-02-12 09:52:18 +0800

    增加注册中心

---
+++ registry-center/src/main/java/com/registry/center/RegistryCenterApplication.java
@@ -0,0 +1,14 @@
+package com.registry.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryCenterApplication.class, args);
+	}
+}
