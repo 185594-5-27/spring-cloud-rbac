33a36ee546d22842929ebdf149484efa83593d8c
282245889@qq.com <575088Wuling>
2018-02-22 21:33:33 +0800

    权限架构生产者（集成菜单与角色的关联关系）

--- rbac-produce/src/main/java/com/produce/sys/controller/RoleController.java
+++ rbac-produce/src/main/java/com/produce/sys/controller/RoleController.java
@@ -1,18 +1,25 @@
 package com.produce.sys.controller;
 
 import com.base.entity.QueryUserRole;
+import com.base.entity.Tree;
 import com.base.entity.UserRole;
+import com.produce.common.base.constant.SystemStaticConst;
 import com.produce.common.base.controller.GenericController;
 import com.produce.common.base.service.GenericService;
+import com.produce.sys.service.TreeService;
 import com.produce.sys.service.UserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /*
 * 类描述：
 * @auther linzf
-* @create 2018/2/6 0006 
+* @create 2018/2/6 0006
 */
 @RestController
 @RequestMapping("/role")
@@ -21,9 +28,32 @@ public class RoleController extends GenericController<UserRole, QueryUserRole> {
     @Autowired
     private UserRoleService userRoleService;
 
+    @Autowired
+    private TreeService treeService;
+
+
     @Override
     protected GenericService<UserRole, QueryUserRole> getService() {
         return userRoleService;
+    }
+
+    @RequestMapping(value = "/loadRoleTree",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String,Object> loadRoleTree(@RequestBody UserRole entity){
+        entity = userRoleService.getUserRoleAssociate(entity);
+        List<Tree> treeList = treeService.query(null);
+        if(entity!=null){
+            for(Tree tree:entity.getTreeList()){
+                treeList.stream().forEach(t->{
+                    if(t.getId()==tree.getId()){
+                        t.setChecked(true);
+                    }
+                });
+            }
+        }
+        Map<String,Object> result = new HashMap<String, Object>();
+        result.put(SystemStaticConst.RESULT, SystemStaticConst.SUCCESS);
+        result.put("data",treeList);
+        return result;
     }
 
 
--- rbac-produce/src/main/java/com/produce/sys/service/UserRoleService.java
+++ rbac-produce/src/main/java/com/produce/sys/service/UserRoleService.java
@@ -2,13 +2,18 @@ package com.produce.sys.service;
 
 
 import com.base.entity.QueryUserRole;
+import com.base.entity.RoleAssociateTree;
+import com.base.entity.Tree;
 import com.base.entity.UserRole;
 import com.produce.common.base.dao.GenericDao;
 import com.produce.common.base.service.GenericService;
+import com.produce.sys.dao.RoleAssociateTreeDao;
 import com.produce.sys.dao.UserRoleDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  *@author linzf
@@ -19,6 +24,9 @@ public class UserRoleService extends GenericService<UserRole, QueryUserRole> {
 	@Autowired
 	@SuppressWarnings("SpringJavaAutowiringInspection")
 	private UserRoleDao userRoleDao;
+	@Autowired
+	@SuppressWarnings("SpringJavaAutowiringInspection")
+	private RoleAssociateTreeDao roleAssociateTreeDao;
 
 	@Override
 	protected GenericDao<UserRole, QueryUserRole> getDao() {
@@ -34,4 +42,45 @@ public class UserRoleService extends GenericService<UserRole, QueryUserRole> {
 		return userRoleDao.getUserRoleAssociate(entity);
 	}
 
+	/**
+	 * 功能描述：删除角色数据
+	 * @param entityList
+	 * @return
+	 * @throws Exception
+	 */
+	@Override
+	public boolean removeBath(List<UserRole> entityList) throws Exception {
+		for(UserRole userRole:entityList){
+			roleAssociateTreeDao.removeTreeByRoleId(userRole);
+		}
+		return super.removeBath(entityList);
+	}
+
+	/**
+	 * 增加角色数据
+	 * @param entity 保存对象
+	 * @return
+	 * @throws Exception
+	 */
+	@Override
+	public boolean save(UserRole entity) throws Exception {
+		entity.packagingTrees(entity.getTreeArray());
+		List<Tree> treeList = entity.getTreeList();
+		boolean success = super.save(entity);
+		for(Tree tree:treeList){
+			roleAssociateTreeDao.save(new RoleAssociateTree(entity.getId(),tree.getId()));
+		}
+		return success;
+	}
+
+	@Override
+	public boolean update(UserRole entity) throws Exception {
+		entity.packagingTrees(entity.getTreeArray());
+		List<Tree> treeList = entity.getTreeList();
+		roleAssociateTreeDao.removeTreeByRoleId(entity);
+		for(Tree tree:treeList){
+			roleAssociateTreeDao.save(new RoleAssociateTree(entity.getId(),tree.getId()));
+		}
+		return super.update(entity);
+	}
 }
